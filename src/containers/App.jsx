ef259e27abfce9e6459482501b721b1debd2da41
evh671 <eyalnahom@gmail.com>
2020-06-01 19:35:06 +0300

    added console.log for fetching users in App.js - ComponentDidMount

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -17,7 +17,10 @@ class App extends Component {
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
+      .then((users) => {
+        console.log("users", users);
+        return this.setState({ robots: users });
+      });
   }
 
   onSearchChange = (e) => {
